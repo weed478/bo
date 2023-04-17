e64c129b46b1f9df288c5de3a457960d32f24efb
miloszdobosz <92726488+miloszdobosz@users.noreply.github.com>
2023-04-17 15:26:12 +0200

    cost function fix

--- solver.py
+++ solver.py
@@ -2,13 +2,13 @@ import numpy as np
 import dask.bag as db
 import itertools
 from problem import *
-from solution_candidates import generate_solution_candidates, generate_solution_candidates_with_rows
-from crossing import cross_solutions, cross_solutions_by_rows
+from solution_candidates import generate_solution_candidates
+from crossing import cross_solutions
 
 
-# What part of space is occupied
+# What part of space is empty
 def fitness_fn(solution: Solution2D):
-    return (solution.cargo != 0).sum() / (
+    return (solution.cargo == 0).sum() / (
         solution.cargo.shape[0] * solution.cargo.shape[1]
     )
 
@@ -22,8 +22,9 @@ def time_fn(solution: Solution2D):
         (package.target_stop for package in solution.problem.packages)
     )
 
-    # TODO: When cargo is 0 it probably doesn't work (?)
-    target_stops_of_fields = np.choose(cargo - 1, target_stops_of_packages) * cargo != -1
+    target_stops_of_fields = (
+        np.choose(solution.cargo - 1, target_stops_of_packages) * solution.cargo != -1
+    )
 
     later_targets_blocking_per_target = np.ndarray(
         # How many fields with y bigger than current have later target stop than t
@@ -40,45 +41,53 @@ def time_fn(solution: Solution2D):
         target_stops_of_fields, later_targets_blocking_per_target
     )
 
-    return np.mean(blocking_per_field)
+    return np.mean(blocking_per_field) / (
+        solution.cargo.shape[0] * solution.cargo.shape[1]
+    )
 
 
-def target_function(solution: Solution2D):
-    return fitness_fn(solution) * time_fn(solution)
-
+def cost_fn(solution: Solution2D):
+    return fitness_fn(solution) + time_fn(solution)
 
 
 if __name__ == "__main__":
     pop_size = 100
+
     prob = Problem2D(
         n_stops=5,
-        cargo_size=(36, 50),
+        cargo_size=(20, 20),
         packages=[
-            Package2D(size=(np.random.choice((1, 2, 4, 6)), np.random.randint(2, 6)), target_stop=np.random.randint(1, 6), deadline=None)
-            for _ in range(300)
+            Package2D(
+                size=(np.random.randint(2, 6), np.random.randint(2, 6)),
+                target_stop=np.random.randint(1, 6),
+                deadline=None,
+            )
+            for _ in range(100)
         ],
         mutation_chance=0.1,
         max_mutation_size=3,
         swap_mutation_chance=0.4,
-        row_height=6,
     )
 
-    # population = list(itertools.islice(generate_solution_candidates(prob), pop_size))
-    population = list(itertools.islice(generate_solution_candidates_with_rows(prob), pop_size))
+    population = list(itertools.islice(generate_solution_candidates(prob), pop_size))
 
     for i in range(10):
         print(f"Generation {i}")
 
-        niceness = np.array(db.from_sequence(population).map(target_fn).compute())
+        cost = np.array(db.from_sequence(population).map(cost_fn).compute())
         print(f"Avg niceness: {np.mean(niceness)}")
 
         parents_a = [
             population[i]
-            for i in np.random.choice(pop_size, pop_size, p=niceness / niceness.sum())
+            for i in np.random.choice(pop_size, pop_size, p=(cost / cost.sum()))
         ]
         parents_b = [
             population[i]
-            for i in np.random.choice(pop_size, pop_size, p=niceness / niceness.sum())
+            for i in np.random.choice(pop_size, pop_size, p=(cost / cost.sum()))
         ]
 
-        population = db.from_sequence(zip(parents_a, parents_b)).map(lambda x: cross_solutions_by_rows(*x)).compute()
+        population = (
+            db.from_sequence(zip(parents_a, parents_b))
+            .map(lambda x: cross_solutions(*x))
+            .compute()
+        )
