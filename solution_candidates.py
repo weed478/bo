56341ecb1379641b9684c2d50dda2a7461aa9274
Dariusz Piwowarski <dariuszpiwowarski@Dariuszs-MacBook-Pro.local>
2023-04-17 14:20:57 +0200

    cross by rows

--- solution_candidates.py
+++ solution_candidates.py
@@ -3,11 +3,12 @@ import numpy as np
 from problem import *
 from crossing import mutate_solution
 
+
 def try_fit(cargo: np.ndarray, pkg: Package2D) -> tuple[int, int] | None:
     dx, dy = pkg.size
     for x in range(cargo.shape[0] - dx + 1):
         for y in range(cargo.shape[1] - dy + 1):
-            if np.all(cargo[x:x+dx, y:y+dy] == 0):
+            if np.all(cargo[x:x + dx, y:y + dy] == 0):
                 return x, y
     return None
 
@@ -22,7 +23,33 @@ def generate_solution_candidates(prob: Problem2D) -> Iterator[Solution2D]:
                 continue
             x, y = top_left
             dx, dy = pkg.size
-            cargo[x:x+dx, y:y+dy] = pkg_i + 1
+            cargo[x:x + dx, y:y + dy] = pkg_i + 1
+        yield Solution2D(cargo=cargo, problem=prob)
+
+
+def try_fit_in_row(cargo: np.ndarray, pkg: Package2D, row_number: int, row_height: int) -> tuple[int, int] | None:
+    dx, dy = pkg.size
+    for x in range(row_height * row_number, row_height * (row_number + 1) - dx + 1):
+        for y in range(cargo.shape[1] - dy + 1):
+            if np.all(cargo[x:x + dx, y:y + dy] == 0):
+                return x, y
+    return None
+
+
+def generate_solution_candidates_with_rows(prob: Problem2D) -> Iterator[Solution2D]:
+    rows = prob.cargo_size[0] // prob.row_height
+    while True:
+        cargo = np.zeros(prob.cargo_size, dtype=np.int32)
+        for pkg_i in np.random.permutation(len(prob.packages)):
+            pkg = prob.packages[pkg_i]
+            for row_i in range(rows):
+                top_left = try_fit_in_row(cargo, pkg, row_i, prob.row_height)
+                if top_left is None:
+                    continue
+                x, y = top_left
+                dx, dy = pkg.size
+                cargo[x:x + dx, y:y + dy] = pkg_i + 1
+                break
         yield Solution2D(cargo=cargo, problem=prob)
 
 
--- solver.py
+++ solver.py
@@ -2,8 +2,8 @@ import numpy as np
 import dask.bag as db
 import itertools
 from problem import *
-from solution_candidates import generate_solution_candidates
-from crossing import cross_solutions
+from solution_candidates import generate_solution_candidates, generate_solution_candidates_with_rows
+from crossing import cross_solutions, cross_solutions_by_rows
 
 
 def fitness_fn(solution: Solution2D):
@@ -11,23 +11,28 @@ def fitness_fn(solution: Solution2D):
 
 
 if __name__ == '__main__':
-    pop_size = 100
+    pop_size = 10
 
     prob = Problem2D(
         n_stops=5,
-        cargo_size=(20, 20),
+        cargo_size=(36, 50),
         packages=[
-            Package2D(size=(np.random.randint(2, 6), np.random.randint(2, 6)), target_stop=np.random.randint(1, 6), deadline=None)
-            for _ in range(100)
+            Package2D(size=(np.random.choice((1, 2, 4, 6)), np.random.randint(2, 6)), target_stop=np.random.randint(1, 6), deadline=None)
+            for _ in range(300)
         ],
         mutation_chance=0.1,
         max_mutation_size=3,
-        swap_mutation_chance=0.4
+        swap_mutation_chance=0.4,
+        row_height=6,
     )
 
-    population = list(itertools.islice(generate_solution_candidates(prob), pop_size))
+    # population = list(itertools.islice(generate_solution_candidates(prob), pop_size))
+    population = list(itertools.islice(generate_solution_candidates_with_rows(prob), pop_size))
 
-    for i in range(10):
+    # print(population[0])
+    # plot_solution(population[0])
+
+    for i in range(1):
         print(f'Generation {i}')
 
         fitness = np.array(db.from_sequence(population).map(fitness_fn).compute())
@@ -36,4 +41,6 @@ if __name__ == '__main__':
         parents_a = [population[i] for i in np.random.choice(pop_size, pop_size, p=fitness / fitness.sum())]
         parents_b = [population[i] for i in np.random.choice(pop_size, pop_size, p=fitness / fitness.sum())]
 
-        population = db.from_sequence(zip(parents_a, parents_b)).map(lambda x: cross_solutions(*x)).compute()
+        cross_solutions_by_rows(parents_a[0], parents_b[1])
+
+        # population = db.from_sequence(zip(parents_a, parents_b)).map(lambda x: cross_solutions_by_rows(*x)).compute()
